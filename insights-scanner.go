72a32db4b5742340fb257b0e9ab758b86b19f1f9
Jeremy Crafts <jcrafts@redhat.com>
2018-01-11 14:59:21 -0500

    test req with dummy data

--- insights-scanner.go
+++ insights-scanner.go
@@ -4,33 +4,30 @@ import (
 	"os"
 	"net/http"
 	"bytes"
-	"flag"
 
 	"log"
 
-	iclient "github.com/RedHatInsights/insights-goapi/client"
-	"github.com/RedHatInsights/insights-goapi/container"
 )
 
 func main() {
-	scanOptions := container.NewDefaultImageMounterOptions()
-	flag.StringVar(&scanOptions.DstPath, "mount_path", scanOptions.DstPath, "Image to scan")
-	flag.StringVar(&scanOptions.Image, "image", scanOptions.Image, "Docker image to scan")
-	flag.Parse()
-	mounter := container.NewDefaultImageMounter(*scanOptions)
-	_, image, _ := mounter.Mount()
+	// scanOptions := container.NewDefaultImageMounterOptions()
+	// flag.StringVar(&scanOptions.DstPath, "mount_path", scanOptions.DstPath, "Image to scan")
+	// flag.StringVar(&scanOptions.Image, "image", scanOptions.Image, "Docker image to scan")
+	// flag.Parse()
+	// mounter := container.NewDefaultImageMounter(*scanOptions)
+	// _, image, _ := mounter.Mount()
 
-	scanner := iclient.NewDefaultScanner()
+	// scanner := iclient.NewDefaultScanner()
 
-	_, out, err := scanner.ScanImage(scanOptions.DstPath, image.ID)
+	// _, out, err := scanner.ScanImage(scanOptions.DstPath, image.ID)
 
-	if err != nil {
-		log.Fatalf("Scan failed %s", err)
-	}
-	log.Printf(string(*out))
+	// if err != nil {
+	// 	log.Fatalf("Scan failed %s", err)
+	// }
+	// log.Printf(string(*out))
 	api := os.Getenv("SCAN_API")
-	jsonStr := []byte(*out)
-	req, err := http.NewRequest("POST", api + "/" + image.ID, bytes.NewBuffer(jsonStr))
+	jsonStr := []byte(`{"test":"test"}`)
+	req, err := http.NewRequest("POST", api + "/1", bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
