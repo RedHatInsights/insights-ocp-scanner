8ac4f807e278701a9b55f87da2fa7d9a7bd21009
Jeremy Crafts <jcrafts@redhat.com>
2018-05-02 16:43:58 -0400

    remove rpm, configs, machine-id generation

--- insights-scanner.go
+++ insights-scanner.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"os"
 	"flag"
-	"path"
-	"io/ioutil"
 	"log"
 	"fmt"
 
@@ -21,14 +18,6 @@ func main() {
 	_, image, _ := mounter.Mount()
 
 	log.Printf(scanOptions.DstPath)
-
-    rhaiDir := path.Join(scanOptions.DstPath, "etc", "redhat-access-insights")
-    machineidPath := path.Join(rhaiDir, "machine-id")
-    os.MkdirAll(rhaiDir, os.ModePerm)
-    err := ioutil.WriteFile(machineidPath, []byte("deeznuts"), 0644)
-    if err != nil {
-        panic(err)
-    }
 
 	scanner := iclient.NewDefaultScanner()
 
